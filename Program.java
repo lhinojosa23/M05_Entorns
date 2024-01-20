760e4e3410f051149a9d5d90d37df8e5aa8ff186
lhinojosa23 <Ferxxo@Fernandos-MacBook-Air.local>
2024-01-20 11:28:37 +0100

    cambio prueba

--- Program.java
+++ Program.java
@@ -4,7 +4,7 @@
  * Program to calculate the area and the perimeter
  * of a circle knowing the radius in meter.
  *
- * Copyright 2020 Joan Sèculi <jseculi@escoladeltreball.org>
+ * Copyright 2020 Joan S?uli <jseculi@escoladeltreball.org>
  *
  * This is free software, licensed under the GNU General Public License v3.
  * See http://www.gnu.org/licenses/gpl.html for more information.
@@ -33,7 +33,7 @@ public class Program {
 	 */
 	public static double perimeter(double radius) {
 
-		return (2 * Math.PI) * radius;
+		return (3 * Math.PI) * radius;
 	}
 
 	/**
